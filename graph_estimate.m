441b61d47d7802fdf3d34b267f9af4969dba0f8c
Daniel Chen <duhe.chen1009@gmail.com>
2024-06-15 02:56:29 -0700

    updated code

--- graph_estimate.m
+++ graph_estimate.m
@@ -2,9 +2,12 @@ labels = ["0h0", "0h1", "0h2", "0h3", "0h4", "0h5", "0h6", "0h7", "0h8", "0h9", 
 avgs = [];
 for i=1:length(labels)
     label = labels(i)
-    load("data/Stochastic/glfvobd verif/results/" + label + "_estimation.mat", "hs", "window_size", "N");
-    slice = (window_size / 2):(N - window_size/2);
-    avgs = [avgs, sum(hs(slice))/length(slice)];
+%     load("data/Stochastic/glfvobd verif/results/" + label + "_estimation.mat", "hs", "window_size", "N");
+%     slice = (window_size / 2):(N - window_size/2);
+%     avgs = [avgs, sum(hs(slice))/length(slice)];
+    load("data/Stochastic/glfvobd verif/mgn_stochastic_" + label + "_glfovbd_buf5000_size100000.mat")
+    h = hurst_estimate(output_noise, "aggvar");
+    avgs = [avgs, h];
 end
 figure;
 hold on;
